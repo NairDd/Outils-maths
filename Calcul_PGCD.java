e47ea8008898c16ebdb9aa6ef3ec6491e148f3c0
NairDd <118678241+NairDd@users.noreply.github.com>
2022-12-25 13:13:00 +0000

    ok

--- Calcul_PGCD.java
+++ Calcul_PGCD.java
@@ -4,7 +4,6 @@ public class Calcul_PGCD {
     public static void main(String[] args) {
         Scanner clavier = new Scanner(System.in);
 
-<<<<<<< HEAD
         System.out.println("Quel est le degré du polynôme P1");
         int degre_polynome_diviseur = clavier.nextInt() + 1;
         System.out.println("Quel est le degré du polynôme P2");
@@ -12,15 +11,6 @@ public class Calcul_PGCD {
         int degre_quotient, deg_reste;
         int[] polynome_diviseur = new int[degre_polynome_diviseur];
         int[] polynome_dividende = new int[degre_polynome_dividende];
-=======
-    System.out.println("Quel est le degré du premier polynôme");
-    int deg_P1 = clavier.nextInt();
-    System.out.println("Quel est le degré du deuxième polynôme");
-    int deg_P2 = clavier.nextInt();
-    int deg_reste;
-    int[] P1 = new int[deg_P1];
-    int[] P2 = new int[deg_P2];
->>>>>>> da711ceda4cc4f347e28411658e871100100d2c0
 
         for(int i =0; i < degre_polynome_diviseur; i++){
             System.out.println("Quelle est la valeur dans P1 de x^ " + i);
@@ -46,7 +36,7 @@ public class Calcul_PGCD {
         //on a le diviseur, le dividende, et le quotient, comment trouver le reste
         //méthode pour trouver le reste, à chaque étape 
 
-        
+
 
     }
 }
